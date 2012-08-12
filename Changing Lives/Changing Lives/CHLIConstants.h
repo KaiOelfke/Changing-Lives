eb5b80ea6f8a41a649d60524401ca022cfd2ebcb
KaiOelfke <k.oelfke@me.com>
2012-08-12 23:01:56 +0200

    added global constants

---
+++ Changing Lives/Changing Lives/CHLIConstants.h
@@ -0,0 +1,11 @@
+//
+//  CHLIConstants.h
+//  Changing Lives
+//
+//  Created by Kai Oelfke on 12.08.12.
+//  Copyright (c) 2012 Kai Oelfke. All rights reserved.
+//
+
+extern NSString * const kLoginUsernameConstant;
+extern NSString * const kLoginAccountConstant;
+extern NSString * const kLoginServiceConstant;
---
+++ Changing Lives/Changing Lives/CHLIConstants.m
@@ -0,0 +1,11 @@
+//
+//  CHLIConstants.m
+//  Changing Lives
+//
+//  Created by Kai Oelfke on 12.08.12.
+//  Copyright (c) 2012 Kai Oelfke. All rights reserved.
+//
+
+NSString * const kLoginUsernameConstant = @"loginUsername";
+NSString * const kLoginAccountConstant = @"DefaultAccount";
+NSString * const kLoginServiceConstant = @"Changing Lives";
